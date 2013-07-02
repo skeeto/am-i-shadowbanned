b4f6258bb0bdda584cd6bcf9657ffee8abc06482
Christopher Wellons <mosquitopsu@gmail.com>
2013-07-01 23:25:11 -0400

    Add working tool.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Reddit shadowban test tool
+
+Test yourself: http://skeeto.github.io/am-i-shadowbanned/
+
+## What's a shadowban?
+
+When a user is shadowbanned they can continue to make posts and leave
+comments without any indication of the ban. However, *everything* they
+do is automatically marked as spam. No one will see any of it until
+the post or comment is graciously freed by a moderator, if ever. To
+the moderator, the post title appears will appear as strikethrough in
+addition to the normal red background.
+
+This tool can test whether an account is shadowbanned, normal, or
+non-existent. It uses [Any Origin](http://anyorigin.com/) to work
+around the cross-domain issue.
---
+++ shadowbanned.js
@@ -0,0 +1,100 @@
+/**
+ * How does it work? For shadowbanned users /user/[user] returns a 404
+ * and /user/[user]/about.json returns a 200. For every other
+ * username, both return either 200 (normal) or 404 (doesn't exist or
+ * deleted). This checker just uses anyorigin.com to test these two
+ * pages.
+ * @param {String} user the user to test.
+ * @param {Function} callback to receive the result.
+ */
+function Checker(user, callback) {
+    this.user = user;
+    this.callback = callback;
+    this.base = null;
+    this.about = null;
+    var url = 'http://www.reddit.com/user/' + user;
+    this.fetch(url, function(data) {
+        this.base = data.status.http_code;
+        this.check();
+    }.bind(this));
+    this.fetch(url + '/about.json', function(data) {
+        this.about = data.status.http_code;
+        this.check();
+    }.bind(this));
+}
+
+/**
+ * Generate a random function name for JSONP.
+ * @returns {String} a randomly-generated function name.
+ */
+Checker.prototype.generateName = function() {
+    var selection = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var name = [];
+    for (var i = 0; i < 32; i++) {
+        name.push(selection[~~(Math.random() * selection.length)]);
+    }
+    return name.join('');
+};
+
+/**
+ * Fetch a URL using anyorigin.com.
+ * @param {String} url
+ * @param {Function} callback is passed the result.
+ */
+Checker.prototype.fetch = function(url, callback) {
+    var s = document.createElement('script');
+    var fname = this.generateName();
+    window[fname] = function(data) {
+        delete window[fname];
+        callback(data);
+    };
+    s.src = 'http://anyorigin.com/get?callback=' + fname + '&url=' +
+        encodeURIComponent(url);
+    document.body.appendChild(s);
+};
+
+/**
+ * Invoke the callback if all of the results have arrived.
+ */
+Checker.prototype.check = function() {
+    if (this.base != null && this.about != null) {
+        if (this.base == 404 && this.about == 200) {
+            this.callback(this.user + ' is shadowbanned');
+        } else if (this.base == 404 && this.about == 404) {
+            this.callback(this.user + ' does not exist');
+        } else {
+            this.callback(this.user + ' looks normal');
+        }
+    }
+};
+
+function Output(node) {
+    this.node = node;
+}
+
+Output.prototype.clear = function() {
+    this.node.innerHTML = '';
+};
+
+Output.prototype.print = function(html) {
+    this.node.innerHTML = html;
+};
+
+window.addEventListener('load', function() {
+    var check = document.getElementById('check');
+    var user = document.getElementById('user');
+    var output = new Output(document.getElementById('output'));
+
+    check.addEventListener('submit', function(event) {
+        event.preventDefault();
+        output.print('Checking ' +  user.value + ' ...');
+        new Checker(user.value, function(result) {
+            output.print(result);
+        });
+        user.blur();
+    });
+
+    user.addEventListener('input', function() {
+        output.clear();
+    });
+});
